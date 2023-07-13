7d8495ac75327d6ee5947e1fc436beb9b0546637
AURELIANO BASSO <71911487+yano79@users.noreply.github.com>
2023-07-13 12:36:01 -0400

    html background colour changed, a few other styling features have been added and also the functionality of editing and deleting a movie.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from sqlalchemy import URL
-from models import Movie, db, Form, Select
+from models import *
 import requests
 import os
 
